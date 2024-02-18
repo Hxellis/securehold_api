0fa36f448caf55160ed43006a0810756521f1d38
dtcz <dylan.tancz@gmail.com>
2024-02-18 18:20:44 +0800

    pending approvals

--- models/models.js
+++ models/models.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 const usersSchema = new mongoose.Schema({
     name: String,
@@ -16,20 +16,20 @@ const usersAppSchema = new mongoose.Schema({
     }
 })
 
-const lockerLocationsSchema = new mongoose.Schema({
-    city: String,
-    address: String,
-    status: { type: Number, enum: [0,1,2]}
-})
+// const lockerLocationsSchema = new mongoose.Schema({
+//     city: String,
+//     address: String,
+//     status: { type: Number, enum: [0,1,2]}
+// })
 
-const lockersSchema = new mongoose.Schema({
-    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "users"},
-    location_id: { type: mongoose.Schema.Types.ObjectId, ref: "locker_locations"},
-    door_status: Boolean,
-    open_count: Number,
-    usage_minutes: Number,
-    last_used: Date
-})
+// const lockersSchema = new mongoose.Schema({
+//     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "users"},
+//     location_id: { type: mongoose.Schema.Types.ObjectId, ref: "locker_locations"},
+//     door_status: Boolean,
+//     open_count: Number,
+//     usage_minutes: Number,
+//     last_used: Date
+// })
 
 const adminsSchema = new mongoose.Schema({
     username: String,
@@ -38,8 +38,16 @@ const adminsSchema = new mongoose.Schema({
     salt: String,
 })
 
+const pendingApprovalsSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    hash: String,
+    salt: String,
+})
+
 export const usersModel = mongoose.model("users", usersSchema)
 export const usersAppModel = mongoose.model("users_app", usersAppSchema)
-export const lockerLocationsModel = mongoose.model("locker_locations", lockerLocationsSchema)
-export const lockersModel = mongoose.model("lockers", lockersSchema)
+// export const lockerLocationsModel = mongoose.model("locker_locations", lockerLocationsSchema)
+// export const lockersModel = mongoose.model("lockers", lockersSchema)
 export const adminsModel = mongoose.model("admins", adminsSchema)
+export const pendingApprovalsModel = mongoose.model("pending_approvals", pendingApprovalsSchema)
--- routes/admins.js
+++ routes/admins.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { adminsModel } from '../models/models.js'
+import { adminsModel, pendingApprovalsModel } from '../models/models.js'
 import crypto from 'crypto'
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
@@ -45,36 +45,93 @@ admins.post("/login", async (req, res) => {
     })
 })
 
-admins.post("/register", async (req, res) => {
-
-    await adminsModel.find({ email: req.body.email })
-    .then( async (existingEmail) => {
-        if (existingEmail.length == 0) {
-            const salt = crypto.randomBytes(16).toString('hex')
-            const hash = crypto.pbkdf2Sync(req.body.password, salt, 1000, 64, `sha512`).toString('hex')
-
-            const newAdmin = {
-                username: req.body.username,
-                email: req.body.email,
-                hash: hash,
-                salt: salt
-            }
+admins.get("/getAllPendingApprovals", async (req, res) => {
+    await pendingApprovalsModel.find()
+    .then ( (data) => {
+        return res.status(200).json({
+            status: 200,
+            msg: "Pending list retrieved",
+            data: data
+        })
+    })
+    .catch ((e) => {
+        return errorMessage(e, res)
+    })
     
-            await adminsModel.create(newAdmin)
-            .then((data) => {
-                return res.status(200).json({
-                    status: 200,
-                    msg: "User inserted",
-                    user: data
-                })
-            })
-            .catch((e) => {
-                return errorMessage(e, res)
-            })
-        }
+})
+
+admins.post("/submitApproval", async (req, res) => {
+
+    const existingEmail = await adminsModel.findOne({ email: req.body.email })
+    .catch( (e) => {
+        return errorMessage(e, res)
+    })
+   
+    if (existingEmail) {
         return res.status(200).json({
             status:400,
-            msg: "Email already exist"
+            msg: "Email already in use"
+        })
+    }
+    
+    const salt = crypto.randomBytes(16).toString('hex')
+    const hash = crypto.pbkdf2Sync(req.body.password, salt, 1000, 64, `sha512`).toString('hex')
+
+    const newAdmin = {
+        username: req.body.username,
+        email: req.body.email,
+        hash: hash,
+        salt: salt
+    }
+
+    await pendingApprovalsModel.create(newAdmin)
+    .then((data) => {
+        return res.status(200).json({
+            status: 200,
+            msg: "Admin sent for approval",
+            user: data
+        })
+    })
+    .catch((e) => {
+        return errorMessage(e, res)
+    })
+})
+
+admins.post("/insertAdmin", async (req, res) => {
+    const pendingAdmin = await pendingApprovalsModel.findOne({ _id: req.body._id }, {_id: false})
+    .catch( (e) => {
+        return errorMessage(e, res)
+    })
+
+    if (pendingAdmin) {
+        await adminsModel.create(pendingAdmin.toObject())
+        .then(async (data) => {
+            await pendingApprovalsModel.deleteOne({ _id: req.body._id })
+    
+            return res.status(200).json({
+                status: 200,
+                msg: "Admin approved",
+                user: data
+            })
+        })
+        .catch((e) => {
+            return errorMessage(e, res)
+        })
+    }
+    else {
+        return res.status(404).json({
+            status: 404,
+            msg: "No pending admin with id:" + req.body._id
+        })
+    }
+})
+
+admins.post("/rejectAdmin", async (req, res) => {
+    await pendingApprovalsModel.deleteOne({ _id: req.body._id })
+    .then(() => {
+        return res.status(200).json({
+            status: 200,
+            msg: "Admin rejected"
         })
     })
     .catch( (e) => {
@@ -116,11 +173,14 @@ admins.post("/sendCode", async (req, res) => {
             </tr>
             <tr>
                 <td style="padding-top:20px; font-size: 40px; font-weight: bold;">
-                    `+ code +`
+                    '${code}'
                 </td>
             </tr>
         </table>
     `})
+    .catch ( (e) => {
+        return errorMessage(e, res)
+    })
 
     return res.status(200).json({
         status: 200,
